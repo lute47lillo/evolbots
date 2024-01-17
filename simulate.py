b84968944dca94582e0668cd12dd4356a4b5253a
lute47lillo <lutelillocs47@gmail.com>
2024-01-17 13:24:06 -0500

    Module D & A2

--- simulate.py
+++ simulate.py
@@ -1,11 +1,18 @@
 import pybullet as p
+import pybullet_data
 import time 
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-# Load the entities generated
-p.loadSDF("box.sdf")
+# Add gravity
+p.setGravity(0,0,-9.8)
+
+# Load the entities generated with pyrosim
+planeId = p.loadURDF("plane.urdf") #Add a floor
+p.loadSDF("boxes.sdf")
 
 for i in range(0,1000):
     p.stepSimulation()
