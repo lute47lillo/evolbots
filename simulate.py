4ed057e42ec60a2c439ff146be448e5d364a90b6
lute47lillo <lutelillocs47@gmail.com>
2024-01-17 12:44:54 -0500

    Assignment1

--- simulate.py
+++ simulate.py
@@ -1,5 +1,10 @@
 import pybullet as p
+import time 
 
 physicsClient = p.connect(p.GUI)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+for i in range(0,1000):
+    p.stepSimulation()
+    time.sleep(0.01)
 p.disconnect()
