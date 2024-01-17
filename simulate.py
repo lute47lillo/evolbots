94303f6f3e068a3c6c30e1a6d79d6cfe5cb20d67
lute47lillo <lutelillocs47@gmail.com>
2024-01-17 13:00:27 -0500

    Module C, A2

--- simulate.py
+++ simulate.py
@@ -4,6 +4,9 @@ import time
 physicsClient = p.connect(p.GUI)
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
+# Load the entities generated
+p.loadSDF("box.sdf")
+
 for i in range(0,1000):
     p.stepSimulation()
     time.sleep(0.01)
